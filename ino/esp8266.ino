701cf629b9c7d0c9e01fd027fa74900f9cacf0f5
Domin <teesdomin@gmail.com>
2021-07-21 20:54:21 +0200

    add ino files

---
+++ ino/esp8266.ino
@@ -0,0 +1,23 @@
+String incomingString = "";
+String text = "info: ";
+int ledPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingString = Serial.readString();
+
+    if (incomingString.length() > 0) {
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(1000);                 
+    }
+  }
+  Serial.println(text);
+  delay(200);
+}
